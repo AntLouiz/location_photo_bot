d0e159243912b76fbb59678d2be94ef00ddd95eb
AntLouiz <luizrodrigo46@hotmail.com>
2018-10-12 19:32:53 -0300

    some messages added to the bot

--- handlers.py
+++ handlers.py
@@ -65,14 +65,47 @@ def support(bot, update):
 
 
 def get_photo(bot, update, post=post):
+    msg = "Certo, agora preciso que envie a localização de onde foi tirada a foto."
+
     post.photo = update.message.photo[-1].file_id
+
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=msg,
+        parse_mode=telegram.ParseMode.MARKDOWN
+    )
 
 
 def get_location(bot, update, post=post):
+    msg = "Pronto, a foto e a localização estão registrados, "
+    msg += "obrigado por colaborar! "
+    msg += emojize(':smile:', use_aliases=True)
+
     if post.photo:
         post.location = update.message.location
         post.save()
         post.clean()
+
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text=msg,
+            parse_mode=telegram.ParseMode.MARKDOWN
+        )
+
+        msg = "Seu registro será avaliado logo logo! E não se esqueça, dirija com cuidado e pare o veículo para os animais."
+        msg += emojize(':innocent:', use_aliases=True)
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text=msg,
+            parse_mode=telegram.ParseMode.MARKDOWN
+        )
+    else:
+        msg = "Ops! Preciso que envie uma foto antes da localização para que eu possa registrar corretamente."
+        bot.send_message(
+            chat_id=update.message.chat_id,
+            text=msg,
+            parse_mode=telegram.ParseMode.MARKDOWN
+        )
 
 
 def default(bot, update):
